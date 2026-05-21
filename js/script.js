b744e3010377d2de31c0b7a9187d4d60139ef2c6
sjhadeveloper <sjhadeveloper@gmail.com>
2026-05-21 18:19:57 +0530

    js file changes resolved

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // console.log('hello');
 const cardsDiv = document.querySelector('.movies-cards');
 
@@ -22,28 +21,3 @@ fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_k
     cardsDiv.innerHTML = cards;
 });
 
-=======
-// console.log('hello');
-const cardsDiv = document.querySelector('.movies-cards');
-
-fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c54dacb95631330e657284be2723541d&page=1')
-.then((res)=> res.json())
-.then((data)=> {
-  let url = "https://media.themoviedb.org/t/p/w440_and_h660_face";
-  const cards =   data.results.slice(0,10).map((user)=>{
-  return `
-  <div class="card">
- <img class="trending-img" src="${url + user.poster_path}">
-  </div>
-  
-  
-  
-  `
-  
-
-    }).join('');
-
-    cardsDiv.innerHTML = cards;
-});
-
->>>>>>> b523968c8494287807698b17fae64c90e9635265
