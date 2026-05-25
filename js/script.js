8684ab503ee7322fbd3a77f8eff748a41f418d10
sjhadeveloper <sjhadeveloper@gmail.com>
2026-05-25 17:52:35 +0530

    API problem fixing

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ const cardsDiv = document.querySelector('.movies-cards');
 fetch('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c54dacb95631330e657284be2723541d&page=1')
 .then((res)=> res.json())
 .then((data)=> {
-  let url = "https://media.themoviedb.org/t/p/w440_and_h660_face";
+  let url = "https://image.tmdb.org/t/p/w500";
   const cards =   data.results.slice(0,10).map((user)=>{
   return `
   <div class="card">
- <img class="trending-img" src="${url + user.poster_path}">
+ <img loading="lazy" class="trending-img" src="${url + user.poster_path}">
   </div>
   
   
